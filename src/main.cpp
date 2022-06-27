c260b75f8adfb736227836e3fe949fa158568253
DanielP540 <podgorskidaniel@o2.pl>
2022-06-27 13:15:54 +0200

    Add Lifeguard app

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 #include "app/calc/calc_app.h"
 #include "app/FindPhone/FindPhone.h"
-//#include "app/gps_status/gps_status.h" // no gps
+#include "app/gps_status/gps_status.h"
 #include "app/kodi_remote/kodi_remote_app.h"
 #include "app/osmand/osmand_app.h"
 #include "app/powermeter/powermeter_app.h"
@@ -19,10 +19,11 @@
 #include "app/calendar/calendar.h"
 #include "app/weather/weather.h"
 #include "app/activity/activity.h"
-//#include "app/tracker/tracker_app.h" // no gps
+#include "app/tracker/tracker_app.h"
 #include "app/fx_rates/fx_rates.h"
-//#include "app/IRController/IRController.h" // no IR
+#include "app/IRController/IRController.h"
 #include "app/alarm_clock/alarm_clock.h"
+#include "app/Lifeguard/Lifeguard_app.h"
 
 #if defined( NATIVE_64BIT )
     /**
@@ -50,25 +51,27 @@ void setup() {
     /**
      * apps here
      */
-    osmmap_app_setup();
-    weather_app_setup();
-    stopwatch_app_setup();
+    
+    Lifeguard_app_setup();
+    //osmmap_app_setup();
+    //weather_app_setup();
+    //stopwatch_app_setup();
     //tracker_app_setup();
-    alarm_clock_setup();
-    activity_app_setup();
+    //alarm_clock_setup();
+    //activity_app_setup();
     calendar_app_setup();
-    astro_app_setup();
+    //astro_app_setup();
     mail_app_setup();
     //gps_status_setup();
     //IRController_setup();
-    sailing_setup();
-    osmand_app_setup();
-    fxrates_app_setup();
-    powermeter_app_setup();
-    FindPhone_setup();
+    //sailing_setup();
+    //osmand_app_setup();
+    //fxrates_app_setup();
+    //powermeter_app_setup();
+    //FindPhone_setup();
     wifimon_app_setup();
-    calc_app_setup();
-    kodi_remote_app_setup();
+    //calc_app_setup();
+    //kodi_remote_app_setup();
     /**
      * post hardware setup
      */
